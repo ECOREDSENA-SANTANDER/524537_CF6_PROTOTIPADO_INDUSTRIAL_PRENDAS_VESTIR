fa7fddd7d202dd89cbaf50e5f4a1423b39fd2444
PABG1990 <pedro19908454@gmail.com>
2026-07-06 15:37:51 -0500

    cfa

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -14,7 +14,7 @@
     figure
       .video
         iframe(width="560" height="315" src="https://www.youtube.com/embed/TlMAZzjg3Ok" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
-      figcaption Video. Introducción - #[em Software] Optitex.  
+      figcaption Video. #[em Software] Optitex.  
 
 </template>
 
